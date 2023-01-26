51dc25e75f27bf15c82156114e60209aad1b611e
chivicoko <vicharmonic@gmail.com>
2023-01-26 08:29:30 +0000

    search function / persisting the input search word

--- src/Context.js
+++ src/Context.js
@@ -3,12 +3,13 @@ import React, { useContext, useEffect, useState } from "react";
 
 const AppContext = React.createContext();
 
-const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=a';
+const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
 const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
 const AppProvider = ({children}) => {
     const [loading, setLoading] = useState(false);
     const [meals, setMeals] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
     const fetchMeals = async (url) => {
         setLoading(true);
@@ -28,11 +29,11 @@ const AppProvider = ({children}) => {
     }
 
     useEffect(() => {
-        fetchMeals(allMealsUrl);
-    }, []);
+        fetchMeals(`${allMealsUrl}${searchTerm}`);
+    }, [searchTerm]);
 
     return (
-        <AppContext.Provider value={{loading, meals}}>
+        <AppContext.Provider value={{loading, meals, setSearchTerm}}>
             {children}
         </AppContext.Provider>
     )
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,10 +2,26 @@ import { React, useState } from 'react';
 import { useGlobalContext } from '../Context';
 
 const Search = () => {
+
+    const [text, setText] = useState('');
+    const {setSearchTerm} = useGlobalContext();
+
+    const handleChange = (e) => {
+        setText(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (text) {
+            setSearchTerm(text);
+            // setText('');      // commenting this line will make the search word to remain on the input interface
+        }
+    }
+
   return (
     <header className='search-container'>
-        <form>
-            <input type='text' placeholder='type favorite meal' className='form-input' />
+        <form onSubmit={handleSubmit}>
+            <input type='text' placeholder='type favorite meal' className='form-input' value={text} onChange={handleChange} />
             <button className='btn' type='submit'>search</button>
             <button className='btn btn-hipster' type='button'>surprise me!</button>
         </form>
